31dfe9821e355d4cf366343e29e36fa90071d1eb
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-06-29 06:25:03 -0300

    Create alg02.rb

---
+++ Algoritmos Ruby/alg02.rb
@@ -0,0 +1,29 @@
+# @param {String} s
+# @param {Integer}
+def roman_to_int(s)
+    roman_values = {
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50, 
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    s.reverse.each_char do |char|
+        current_value = roman_values[char]
+        
+        if current_value < prev_value
+            total -= current_value
+        else
+            total += current_value
+        end
+        prev_value = current_value
+    end
+    total
+end
+
