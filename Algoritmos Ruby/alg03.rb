dc0f56c06f8098bb72098a82c2abc325c787f04e
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-06-29 07:36:16 -0300

    Update alg03.rb

--- Algoritmos Ruby/alg03.rb
+++ Algoritmos Ruby/alg03.rb
@@ -1,6 +1,7 @@
 # @param {Integer[]} nums
 # @param {Integer} val
 # @return {Integer}
+=begin
 def remove_element(nums, val)
     k = 0
     nums.each do |num|
@@ -16,4 +17,16 @@ def remove_element(nums, val)
   val = 2
   k = remove_element(nums, val)
   puts "k = #{k}, nums = #{nums[0...k].join(", ")}"
+=end
+
+def remove_element(nums, val)
+  nums.delete(val)
+  nums.length
+end
+
+nums = [0, 1, 2, 2, 3, 0, 4, 2]
+val = 2
+remove_element(nums, val)
+puts "val = #{val}, nums = #{nums[0...nums.length].join(", ")}"
+
   
